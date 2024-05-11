f8f34d63626c6122398de270c80fb1aa45dc89c2
WirusDev <zlarin63@gmail.com>
2024-05-11 16:53:41 +0200

    v1.0.2 Analytics added

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import { SpeedInsights } from "@vercel/speed-insights/react";
+import { Analytics } from "@vercel/analytics/react";
 import "./index.css";
 import { CookiesProvider } from "react-cookie";
 import "tw-elements-react/dist/css/tw-elements-react.min.css";
@@ -10,6 +11,7 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
   <CookiesProvider>
     <React.StrictMode>
       <App />
+      <Analytics />
       <SpeedInsights />
     </React.StrictMode>
   </CookiesProvider>
